c5ec9653281e71ffafe9d60ab7d7b048b079032f
MeSanthosh <santhosh.sundaram1993@gmail.com>
2020-11-19 12:34:23 +0530

    commit and pushed

--- java projects/src/array.java
+++ java projects/src/array.java
@@ -10,8 +10,8 @@ public class array {
 		int[] values;
 		values=new int[3];
 		values[0]=1;
-		values[1]=3;
-		values[2]=5;
+		values[1]=2;
+		values[2]=6;
 		//values[4]=5;
 		
 		for(int i=0;i<values.length;i++)
