048859729b4a117bb7dd3744d224964edca55685
MeSanthosh <santhosh.sundaram1993@gmail.com>
2020-11-21 19:30:29 +0530

    Dinesh_Kt

--- java projects/src/OOPSpart1/localandglobalvariable.java
+++ java projects/src/OOPSpart1/localandglobalvariable.java
@@ -13,18 +13,18 @@ public class localandglobalvariable {
 		//to call global variable we need to create obj beacse it was a non static variables 
 		localandglobalvariable obj=new localandglobalvariable();
 		System.out.println(obj.age);
-		int w=obj.net(); //incase of using return in method we need to save value in one more variable like this..
-		System.out.print(w); 
+		//int w=obj.net(); //incase of using return in method we need to save value in one more variable like this..
+		//System.out.print(w); 
 		
 		
 
 	}
-	public int net()
+	public void net()
 	{
 		int x=10;
 		int y=4;
 		int z=x+y;
-		return z;
-		//System.out.print(z);
+		//return z;
+		System.out.print(z);
 	}
 }
